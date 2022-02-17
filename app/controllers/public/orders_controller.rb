82d8f1abb096de0f97bb9cabdf287cddf04e02e0
yakushimizu <97076369+yakushimizu@users.noreply.github.com>
2022-02-17 02:26:28 +0000

    orders_new

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,3 +1,14 @@
 class Public::OrdersController < ApplicationController
- 
+
+ def new
+   @order = Order.new
+   @end_user = current_end_user
+   @address = Address.new
+ end
+
+ private
+
+ def order_params
+   params.require(:order).permit(:postcode, :address, :name, :shipping_cost, :total_payment, :payment_method, :order_status)
+ end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,6 @@
 class Address < ApplicationRecord
 belongs_to :end_user
+ def full_address
+    '〒' + postcode + ' ' + address + ' ' + name
+ end
 end
