8740c3bc5c4fc27bd12adc442b59804b9eb54497
EC2 Default User <ec2-user@ip-172-31-45-240.ec2.internal>
2022-02-17 10:46:38 +0900

    [Add]ログイン権限の追加、新規登録時の保存カラムの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
+ before_action :authenticate_user!, except: [:top,:about,:index,:show]
+ before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_up_path_for(resource)
   end_users_mypage(resource)
   end
 
+
+ protected
+
+ def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up,keys: [:last_name,:first_name,:last_name_kana,:first_name_kana,:postcode,:address,:phone_number])
+ end
 
 end
