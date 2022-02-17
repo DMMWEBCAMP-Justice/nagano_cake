b17e42455d042aa78adf6b92c10cb8e69f1daf37
EC2 Default User <ec2-user@ip-172-31-45-240.ec2.internal>
2022-02-17 17:10:25 +0900

    [Add]編集ボタン追加、遷移先確認済み

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,28 @@
 class Admin::ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+    @item.genre_id =Genre.all
+  end
+
+  def create
+     @item = Item.new(item_params)
+    　@item.genre_id =Genre.all
+     if @item.save
+      redirect_to admin_item_path(@item.id)
+     else
+      render :new
+     end
+  end
+
+
+
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :price, :genre_id, :item_status, :image_id, :introduction) 
+  end
+
 end
