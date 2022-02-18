11fb9453016d1797ee43b28c98e8ab59b9371a72
EC2 Default User <ec2-user@ip-172-31-31-106.ec2.internal>
2022-02-18 05:57:59 +0000

    [Add]order index show

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,11 +1,12 @@
 class Public::OrdersController < ApplicationController
 
   def index
-    @orders = Order.all
+    @orders = current_end_user.orders
   end
 
   def show
-    @order = Oder.find(params[id])
+    @order = Order.find(params[id])
+    @order_details = current_end_user.order_details
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,8 @@ class Order < ApplicationRecord
   belongs_to :end_user
   has_many :order_details, dependent: :destroy
   
+  #小計を求めるメソッド？？
+  def subtotal
+    price * amou
+  
 end
