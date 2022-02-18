40b93d56739aab3908c27238249aa82523cb9abb
yakushimizu <97076369+yakushimizu@users.noreply.github.com>
2022-02-18 03:57:34 +0000

    全ページに対応header

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.web_console.whitelisted_ips = '106.180.7.63'
+  config.web_console.whitelisted_ips = '106.180.7.11'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -79,5 +79,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 config.hosts << "9d4c54a17b7146bb89af638acecd7810.vfs.cloud9.us-east-1.amazonaws.com"
 config.hosts << "cf4fd20960e1417b9cad591d45dca4fb.vfs.cloud9.us-east-1.amazonaws.com"
+config.hosts << "5cf0444a6f524972bec9d71a709aed49.vfs.cloud9.us-east-1.amazonaws.com"
 
 end
