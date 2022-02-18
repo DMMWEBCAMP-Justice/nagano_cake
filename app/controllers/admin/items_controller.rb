ada8e6d6877f51fbd5c4d26d586361e849554cdd
kokuniho <96281192+kokuniho@users.noreply.github.com>
2022-02-18 01:31:13 +0000

    管理者（index,show,homesコントローラ）作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,12 +2,10 @@ class Admin::ItemsController < ApplicationController
 
   def new
     @item = Item.new
-    @item.genre_id =Genre.all
   end
 
   def create
      @item = Item.new(item_params)
-    　@item.genre_id =Genre.all
      if @item.save
       redirect_to admin_item_path(@item.id)
      else
@@ -15,7 +13,30 @@ class Admin::ItemsController < ApplicationController
      end
   end
 
-
+  def index
+     @items=Item.page(params[:page])
+     
+  end
+  
+  def show
+     @item = Item.find(params[:id])
+     @order_details = @item.order_details
+  end
+  
+  def edit
+    @item=Item.find(params[:id])
+      redirect_to "edit"
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to admin_item_path(@item.id)
+    else
+      render :edit
+    end
+  end
 
 
 
