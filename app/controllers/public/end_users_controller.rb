aee6cd6b11719698a472f68e93010801b9e76969
EC2 Default User <ec2-user@ip-172-31-31-106.ec2.internal>
2022-02-19 06:39:00 +0000

    [Add]admin user index edit show

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -3,5 +3,17 @@ class Public::EndUsersController < ApplicationController
   def show
     @user = current_end_user
   end
+# 管理者側で編集機能を追加する際に必要な記述です。これ書いたら正常に動作しました。
+  def update
+    @end_user = EndUser.find(params[:id])
+    @end_user.update(end_user_params)
+    redirect_to admin_end_user_path(@end_user)
+  end
+
+  private
+
+  def end_user_params
+    params.require(:end_user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :phone_number, :email)
+  end
 
 end
