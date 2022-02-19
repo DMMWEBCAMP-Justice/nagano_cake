9abb8ca095a2ed7ed007c4bb8361a29264d2f661
kokuniho <96281192+kokuniho@users.noreply.github.com>
2022-02-19 12:31:35 +0000

    admin/new,edit修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -31,6 +31,7 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+    
     @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to admin_item_path(@item.id)
--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,48 +1,51 @@
 class Public::CartItemsController < ApplicationController
- 
+
  def index
    @cart_items = CartItem.all
-   @caret_item = CartItem.find(params[:id])
+   # @cart_items = current_end_user.cart_items
  end
- 
+
  # def create
  #    @cart_item =CartItem.find_by(id: @item.cart_item_id)
  #     if @cart_item.save
  #  1. 追加した商品がカート内に存在するかの判別
  #    存在した場合
  #      2. カート内の個数をフォームから送られた個数分追加する
-    
-      
  #    else
  #      @cart_item = CartItem.new
  # end
- 
+
  def update
+   @cart_item = Cartitem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to cart_item_path(@cart_item.id)
+       
+    end
  end
- 
+
  def destroy
-  @cart_item = Cart_item.find(params[:id])
-  if @cart_item.end_user_id == current_user.id
+  @cart_item = Cartitem.find(params[:id])
+  if @end_user == current_user.id
      @cart_item.destroy
      redirect_to cart_items_path
   end
  end
- 
+
  def destroy_all
-    @cart_item = CartItem.find(params[:id])
-    if @cart_item.end_user_id == current_user.id
+      @cart_item = CartItem.all
+      if @end_user == current_user.id
       @cart_item.destroy_all
       redirect_to cart_items_path
-    end
+      end
  end
- 
-  
 
- 
- 
+
+
+
+
    private
   def cart_item_params
       params.require(:cart_item).permit(:item_id, :amount)
   end
-  
+
 end
