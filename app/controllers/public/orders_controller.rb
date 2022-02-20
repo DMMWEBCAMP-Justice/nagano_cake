ce49194139abbf3e08d7308aaa27819323ac1a71
yakushimizu <97076369+yakushimizu@users.noreply.github.com>
2022-02-20 09:14:03 +0000

    途中までですが

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,18 @@
 class Public::OrdersController < ApplicationController
 
+  def new
+    @order = Order.new
+    @address = Address.new
+  end
+
+  def confirm
+    @order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+    @order.postcode = @address.postcode
+    @order.address = @address.address
+    @order.name = @address.name
+  end
+
   def index
     @orders = current_end_user.orders
   end
@@ -7,6 +20,12 @@ class Public::OrdersController < ApplicationController
   def show
     @order = Order.find(params[id])
     @order_details = current_end_user.order_details
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:address, :postcode, :name, :payment_method)
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :end_user
   has_many :order_details, dependent: :destroy
+  
+  enum payment_method: { credit_card: 0, transfer: 1 }
 
   def subtotal
     price * amou
