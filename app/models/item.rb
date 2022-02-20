b8af8b2ed4cd71f5aa22e4dfb7b1895ddb9a5f2f
kokuniho <96281192+kokuniho@users.noreply.github.com>
2022-02-20 01:15:28 +0000

    修正4

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,9 @@ class Item < ApplicationRecord
   has_one_attached :image
 
 
-  def get_image
-    image
-  end
+  # def get_image
+  #   image
+  # end
   
   def with_tax_price
     (price * 1.1).floor
