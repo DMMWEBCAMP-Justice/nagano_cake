cbe654d024a81eda348058a8d6457d545872640a
yakushimizu <97076369+yakushimizu@users.noreply.github.com>
2022-02-22 14:41:42 +0000

    updateの自動更新機能を付けました

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,10 +2,15 @@ class Admin::OrderDetailsController < ApplicationController
 
   def update
     @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
     @order_detail.update(order_detail_params)
-    redirect_to admin_order_path(@order_detail.order)
+    @order.update(order_status: "making") if @order.order_details.where(making_status: "making").count == @order.order_details.count
+    @order.update(order_status: "prepare") if @order.order_details.where(making_status: "finished").count == @order.order_details.count
+    redirect_to request.referer
   end
-  
+
+
+
   private
 
   def order_detail_params
